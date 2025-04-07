a90c7f362efe7951b8f2ade73caeaf08e565effc
Cauã Motta <ocauamotta@gmail.com>
2025-04-07 10:55:24 -0300

    Feat: Create card test

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,87 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { ThemeProvider } from 'styled-components'
+
+import Card from '.'
+
+import { darkTheme } from '../../themes'
+
+vi.mock('../Modal', () => ({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  default: ({ isOpen, title }: any) =>
+    isOpen ? <div data-testid="modal">Modal is open: {title}</div> : null
+}))
+
+const projectMock: Project = {
+  id: 1,
+  title: 'Test title',
+  resume: 'Test description',
+  image: 'test.png',
+  linkRepo: 'https://github.com/test',
+  published: false,
+  tecnologies: ['React', 'Typescript']
+}
+
+const certificateMock: Certificate = {
+  id: 1,
+  title: 'Test title',
+  date: '01/01/2000',
+  school: 'test',
+  link: 'https://test.com'
+}
+
+describe('Card - Project', () => {
+  beforeEach(() => {
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <Card type="project" content={projectMock} />
+      </ThemeProvider>
+    )
+  })
+
+  test('Should render the project card', () => {
+    expect(screen.getByAltText('Test title')).toHaveAttribute('src', 'test.png')
+    expect(screen.getByText('Test title')).toBeInTheDocument()
+    expect(screen.getByText('Test description')).toBeInTheDocument()
+    expect(screen.getByText('Ver detalhes')).toBeInTheDocument()
+  })
+
+  test('Should open the modal when clicking on the button', async () => {
+    const button = screen.getByText('Ver detalhes')
+    await userEvent.click(button)
+
+    expect(screen.getByTestId('modal')).toBeInTheDocument()
+    expect(screen.getByText('Modal is open: Test title')).toBeInTheDocument()
+  })
+
+  test('Should check if the link has the correct href', () => {
+    const link = screen.getByRole('link', { name: /ver no github/i })
+    expect(link).toHaveAttribute('href', projectMock.linkRepo)
+  })
+})
+
+describe('Card - Certificate', () => {
+  beforeEach(() => {
+    render(
+      <ThemeProvider theme={darkTheme}>
+        <Card type="certificate" content={certificateMock} />
+      </ThemeProvider>
+    )
+  })
+
+  test('Should render the certificate card', () => {
+    screen.debug()
+    expect(screen.getByAltText('Test title')).toHaveAttribute(
+      'src',
+      '/assets/test-light.svg'
+    )
+
+    expect(screen.getByText('Test title')).toBeInTheDocument()
+    expect(screen.getByText('01/01/2000')).toBeInTheDocument()
+  })
+
+  test('Should check if the link has the correct href', () => {
+    const link = screen.getByRole('link', { name: /ver certificado/i })
+    expect(link).toHaveAttribute('href', certificateMock.link)
+  })
+})
